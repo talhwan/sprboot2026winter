1f8dc8993fb366b66e1988446da507527fc0be04
01 <talhwan@hotmail.com>
2025-12-30 11:40:57 +0900

    20251230 2
    ctrl-, front-

--- src/main/java/com/example/demo/controller/DefaultRestController.java
+++ src/main/java/com/example/demo/controller/DefaultRestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.ParamTest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,10 +13,18 @@ import java.util.Map;
 public class DefaultRestController {
 
     @RequestMapping("/test")
-    public Map<String, Object> test() {
+    public Map<String, Object> test(@RequestParam Map<String, Object> params) {
         Map<String, Object> map = new HashMap<>();
-        map.put("test1", 112233);
+        map.put("test1", params.get("a"));
         map.put("test2", 112233);
+        return map;
+    }
+
+    @RequestMapping("/test2")
+    public Map<String, Object> test2(ParamTest params) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("param1", params.getParam1());
+        map.put("param2", params.getParam2());
         return map;
     }
 
